7afce1065a7dda6a994f0b5ff2c526d0d7e094b6
Maxime Imbeau <maxime.imbeau@hotmail.com>
2025-01-24 09:52:51 -0500

    fix compilation

--- labo1/src/main/java/ulaval/glo2003/HealthResource.java
+++ labo1/src/main/java/ulaval/glo2003/HealthResource.java
@@ -2,12 +2,17 @@ package ulaval.glo2003;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("")
 public class HealthResource {
+
     @GET
+    @Path("health")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response health() {
-        return Response.ok("OK").build();
+        return Response.ok(new HealthResponse("ok")).build();
     }
 }
--- labo1/src/main/java/ulaval/glo2003/Main.java
+++ labo1/src/main/java/ulaval/glo2003/Main.java
@@ -1,21 +1,22 @@
 package ulaval.glo2003;
 
-import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.io.IOException;
 import java.net.URI;
 
 public class Main {
+    public static final String BASE_URI = "http://0.0.0.0:8080/";
 
-    public static void main(String[] args) throws IOException {
-        HealthResource healthResource = new HealthResource();
-        ResourceConfig resourceConfig = new ResourceConfig()
-                .register(healthResource);
-        URI uri = URI.create("http://localhost:8080/");
+    public static void startServer() {
+        final ResourceConfig rc = new ResourceConfig()
+                .register(new HealthResource());
 
-        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, resourceConfig);
-        server.start();
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    public static void main(String[] args) {
+        startServer();
+        System.out.printf("Jersey app started with endpoints available at %s%n", BASE_URI);
     }
 }
